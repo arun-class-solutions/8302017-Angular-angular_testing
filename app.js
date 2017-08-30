e4356e475004203d34f97a5cc36f7c320bb1aedc
Arun Sood <arsood@gmail.com>
2017-08-30 11:47:39 -0700

    HTTP request to get all users with console.log

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 var app = angular.module("testApp", []);
 
 // Attach controller to module to enable our app's functionality
-app.controller("testController", function($scope) {
+app.controller("testController", function($scope, $http) {
     // Click event handler for button
     $scope.showInput = function() {
         // Show in the console the user input from the data-bound input field
         console.log($scope.userInput);
     }
 
-    $scope.users = [
-        {
-            firstname: "Bob",
-            lastname: "Jones",
-            username: "bjones",
-            email: "bjones@gmail.com"
-        },
-        {
-            firstname: "John",
-            lastname: "Smith",
-            username: "jsmith",
-            email: "jsmith@gmail.com"
-        }
-    ];
+    // Use AJAX to pull user data from the API and attach them to the scope
+    $http
+    .get("http://myapi-profstream.herokuapp.com/api/bc9626/persons")
+    .then(function(users) {
+        console.log(users);
+    }, function(err) {
+        console.log(err);
+    });
+
+    // $scope.users = [
+    //     {
+    //         firstname: "Bob",
+    //         lastname: "Jones",
+    //         username: "bjones",
+    //         email: "bjones@gmail.com"
+    //     },
+    //     {
+    //         firstname: "John",
+    //         lastname: "Smith",
+    //         username: "jsmith",
+    //         email: "jsmith@gmail.com"
+    //     }
+    // ];
 });
