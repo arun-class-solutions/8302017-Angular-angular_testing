469d1456df4a007d15035d65697b950f6939c862
Arun Sood <arsood@gmail.com>
2017-08-31 12:51:45 -0700

    Refactor factory to use the resource module

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-var app = angular.module("testApp", []);
+var app = angular.module("testApp", ["ngResource"]);
+
+app.factory("TestFactory", function() {
+    var factory = {};
+
+    factory.sayHello = function() {
+        alert("Hello World!");
+    }
+
+    return factory;
+});
+
+// Person "Model"
+app.factory("Person", function($resource) {
+    // var factory = {};
+
+    // factory.getAll = function() {
+    //     return $http
+    //     .get("http://myapi-profstream.herokuapp.com/api/bc9626/persons");
+    // }
+
+    // return factory;
+
+    // Return an object (using the $resource service) that has CRUD methods for the /persons resource
+    return $resource("http://myapi-profstream.herokuapp.com/api/bc9626/persons/:id", {
+        id: "@id"
+    }, {
+        update: {
+            method: "PUT"
+        }
+    });
+});
 
 // Attach controller to module to enable our app's functionality
-app.controller("testController", function($scope, $http) {
+app.controller("testController", function($scope, TestFactory, Person) {
+    // Call sayHello from OUR factory (service)
+    TestFactory.sayHello();
+
     // Click event handler for button
     $scope.showInput = function() {
         // Show in the console the user input from the data-bound input field
@@ -9,14 +43,22 @@ app.controller("testController", function($scope, $http) {
     }
 
     // Use AJAX to pull user data from the API and attach them to the scope
-    $http
-    .get("http://myapi-profstream.herokuapp.com/api/bc9626/persons")
-    .then(function(users) {
-        // Assign data coming from the API to the scope
-        $scope.users = users.data;
+    // $http
+    // .get("http://myapi-profstream.herokuapp.com/api/bc9626/persons")
+    
+    // Use the resource we set up above to query the API
+    Person
+    .query(function(users) {
+        $scope.users = users;
     }, function(err) {
         console.log(err);
     });
+    // .then(function(users) {
+    //     // Assign data coming from the API to the scope
+    //     $scope.users = users.data;
+    // }, function(err) {
+    //     console.log(err);
+    // });
 
     // $scope.users = [
     //     {
